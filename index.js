9cb114002bcb2d0b7a920747b6e008813a178ad5
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-05-20 20:13:08 +0200

    importati ChatAnthropic e HumanMessage per farmi raccontare le barzellette da Claude

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { cowsay,  } from "cowsayjs";
+import { ChatAnthropic } from "@langchain/anthropic";
+import { HumanMessage } from "langchain";
 console.log('hello boolean');
 
 const dummyArr = ['sopra', 'la', 'panca', 'la', 'capra', 'campa'];
@@ -8,14 +10,22 @@ const CMDparams = process.argv;
 
 const myKey = envVariables.CLAUDE_API_KEY;
 
-
 //stampo il terzo elemento dell'array process.argv
 // console.log(CMDparams[2]);
 
 const random = Math.floor(Math.random()*6);
-
-
 console.log(cowsay(dummyArr[random]));
+
+const model = new ChatAnthropic({
+    model: 'claude-sonnet-4-6',
+    apiKey: myKey
+});
+
+model.invoke([
+    new HumanMessage('Mi racconti una barzelletta zozza?')
+]).then(aiResp => {
+    console.log(aiResp.content);
+})
 
 
 
