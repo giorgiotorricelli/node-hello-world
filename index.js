abbf07b7689e2a8ca7dab9e3c67a2801b91b0f11
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-05-20 19:53:44 +0200

    importata la libreria cowsay e modificato il codice per loggare una stringa casuale dell'array detta dalla mucca

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+import { cowsay,  } from "cowsayjs";
 console.log('hello boolean');
 
+const dummyArr = ['sopra', 'la', 'panca', 'la', 'capra', 'campa'];
+
+const envVariables = process.env;
 const CMDparams = process.argv;
 
+const myKey = envVariables.CLAUDE_API_KEY;
+
+
 //stampo il terzo elemento dell'array process.argv
-console.log(CMDparams[2]);
+// console.log(CMDparams[2]);
+
+const random = Math.floor(Math.random()*6);
+
+
+console.log(cowsay(dummyArr[random]));
+
 
 
